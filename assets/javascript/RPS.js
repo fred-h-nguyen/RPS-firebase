c553053e14fdaff026540c8a61fd8b2880559d86
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-07-27 23:13:27 -0500

    initialize firebase

---
+++ assets/javascript/RPS.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    var firebaseConfig = {
+        apiKey: "AIzaSyAoQrxWc_4Fwkb9SG1dhir6h46g4uNcT24",
+        authDomain: "countdown-b87c9.firebaseapp.com",
+        databaseURL: "https://countdown-b87c9.firebaseio.com",
+        projectId: "countdown-b87c9",
+        storageBucket: "",
+        messagingSenderId: "1036603245427",
+        appId: "1:1036603245427:web:7965de60192535dd"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+
+      var database = firebase.database();
+
+      var player1 ="";
+      var player2 ="";
+
+
+
+
+
+
+
+
+
+})
